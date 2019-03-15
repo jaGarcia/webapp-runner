dae3af37f5cdb6efcd191640d9e2cf7790c4a1ab
John Garcia <g3.andrew@gmail.com>
2019-03-15 18:14:39 -0400

    test5

--- main/src/main/java/webapp/runner/launch/Main.java
+++ main/src/main/java/webapp/runner/launch/Main.java
@@ -72,7 +72,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Init'ing custom web-app runner version! test4");
+        System.out.println("Init'ing custom web-app runner version! test5");
 
         CommandLineParams commandLineParams = new CommandLineParams();
 
